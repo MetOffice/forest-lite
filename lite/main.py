24a699331e027af196a84d4886a8cc28e31288bc
andrewgryan <andrew.ryan@metoffice.gov.uk>
2020-08-10 12:11:39 +0100

    port configuration to uvicorn server

--- lite/main.py
+++ lite/main.py
@@ -6,6 +6,12 @@ from fastapi import Response, Request
 from fastapi.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
 import bokeh.resources
+import bokeh.palettes
+from bokeh.core.json_encoder import serialize_json
+import yaml
+import lib
+import lib.config
+import lib.palette
 
 
 app = fastapi.FastAPI()
@@ -14,6 +20,20 @@ app.mount("/static", StaticFiles(directory=static_dir), name="static")
 
 templates_dir = os.path.join(os.path.dirname(__file__), "templates")
 templates = Jinja2Templates(directory=templates_dir)
+
+
+CONFIG = None  # TODO: Remove global variable
+
+
+@app.on_event("startup")
+async def startup_event():
+    global CONFIG
+
+    # Configure application
+    args = parse_args()
+    with open(args.config_file) as stream:
+        data = yaml.safe_load(stream)
+        CONFIG = lib.config.Config(**data)
 
 
 @app.get("/")
@@ -26,16 +46,30 @@ async def root(request: Request):
     return templates.TemplateResponse("index.html", context)
 
 
-@app.get("/hello")
-async def endpoint(response: Response):
+@app.get("/datasets")
+async def datasets(response: Response):
     response.headers["Cache-Control"] = "max-age=31536000"
-    return {"message": "Hello, Fetch API!"}
+    return {"names": sorted(dataset.label for dataset in CONFIG.datasets)}
+
+
+@app.get("/image/{dataset_name}")
+async def image(dataset_name):
+    return {}
+
+
+def parse_args():
+    """Command line interface"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--port", type=int, default=8888)
+    parser.add_argument("config_file")
+    return parser.parse_args()
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--port", type=int, default=8888)
-    args = parser.parse_args()
+    # Parse command line arguments
+    args = parse_args()
+
+    # Start server
     uvicorn.run("main:app", port=args.port)
 
 
