fdaa6d600eaa4abebd5a760b0e0d845322f0675c
andrewgryan <andrew.ryan@metoffice.gov.uk>
2020-08-12 09:58:21 +0100

    move reducer outside of main

--- lite/static/script.js
+++ lite/static/script.js
@@ -5,6 +5,44 @@ let providers = {
     'Voyager': 'https://d.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{Z}/{X}/{Y}.png'
 }
 
+
+// ReduxJS
+let reducer = (state = "", action) => {
+    switch (action.type) {
+        case 'SET_DATASET':
+            return Object.assign({}, state, {dataset: action.payload})
+        case 'SET_DATASETS':
+            return Object.assign({}, state, {datasets: action.payload})
+        case 'SET_URL':
+            return Object.assign({}, state, {url: action.payload})
+        case 'SET_PALETTE':
+            return Object.assign({}, state, {palette: action.payload})
+        case 'SET_PALETTES':
+            return Object.assign({}, state, {palettes: action.payload})
+        case 'SET_PALETTE_NAME':
+            return Object.assign({}, state, {palette_name: action.payload})
+        case 'SET_PALETTE_NAMES':
+            return Object.assign({}, state, {palette_names: action.payload})
+        case 'SET_PALETTE_NUMBER':
+            return Object.assign({}, state, {palette_number: action.payload})
+        case 'SET_PALETTE_NUMBERS':
+            return Object.assign({}, state, {palette_numbers: action.payload})
+        case 'SET_LIMITS':
+            return Object.assign({}, state, {limits: action.payload})
+        case 'SET_TIMES':
+            return Object.assign({}, state, {times: action.payload})
+        case 'SET_TIME_INDEX':
+            return Object.assign({}, state, {time_index: action.payload})
+        case 'FETCH_IMAGE':
+            return Object.assign({}, state, {is_fetching: true, image_url: action.payload})
+        case 'FETCH_IMAGE_SUCCESS':
+            return Object.assign({}, state, {is_fetching: false})
+        default:
+            return state
+    }
+}
+
+
 let main = function() {
     // Geographical map
     let xdr = new Bokeh.Range1d({ start: 0, end: 1e6 })
@@ -27,42 +65,6 @@ let main = function() {
     let renderer = new Bokeh.TileRenderer({tile_source: tile_source})
     figure.renderers = figure.renderers.concat(renderer)
     Bokeh.Plotting.show(figure, "#map-figure")
-
-    // ReduxJS
-    let reducer = (state = "", action) => {
-        switch (action.type) {
-            case 'SET_DATASET':
-                return Object.assign({}, state, {dataset: action.payload})
-            case 'SET_DATASETS':
-                return Object.assign({}, state, {datasets: action.payload})
-            case 'SET_URL':
-                return Object.assign({}, state, {url: action.payload})
-            case 'SET_PALETTE':
-                return Object.assign({}, state, {palette: action.payload})
-            case 'SET_PALETTES':
-                return Object.assign({}, state, {palettes: action.payload})
-            case 'SET_PALETTE_NAME':
-                return Object.assign({}, state, {palette_name: action.payload})
-            case 'SET_PALETTE_NAMES':
-                return Object.assign({}, state, {palette_names: action.payload})
-            case 'SET_PALETTE_NUMBER':
-                return Object.assign({}, state, {palette_number: action.payload})
-            case 'SET_PALETTE_NUMBERS':
-                return Object.assign({}, state, {palette_numbers: action.payload})
-            case 'SET_LIMITS':
-                return Object.assign({}, state, {limits: action.payload})
-            case 'SET_TIMES':
-                return Object.assign({}, state, {times: action.payload})
-            case 'SET_TIME_INDEX':
-                return Object.assign({}, state, {time_index: action.payload})
-            case 'FETCH_IMAGE':
-                return Object.assign({}, state, {is_fetching: true, image_url: action.payload})
-            case 'FETCH_IMAGE_SUCCESS':
-                return Object.assign({}, state, {is_fetching: false})
-            default:
-                return state
-        }
-    }
 
     let colorPaletteMiddleware = store => next => action => {
         console.log(action)
