510bb088e23a93d81758bad70cdf2dca82224b0d
andrewgryan <andrew.ryan@metoffice.gov.uk>
2020-08-24 11:43:58 +0100

    replace call to /google_limits with consts

--- lite/tiling.test.js
+++ lite/tiling.test.js
@@ -6,3 +6,13 @@ test('interp1d interpolates midpoint of line segment', () => {
     let expected = 0.5
     expect(actual).toEqual(expected)
 })
+
+
+test("WEB_MERCATOR_EXTENT", () => {
+    const actual = tiling.WEB_MERCATOR_EXTENT
+    const expected = {
+        x: [ -20037508.342789244, 20037508.342789244 ],
+        y: [ -20037508.342789255, 20037508.342789244 ]
+    }
+    expect(actual).toEqual(expected)
+})
