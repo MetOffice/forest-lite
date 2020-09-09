653917f961b12b8239e062f07255b800e2f178a6
andrewgryan <andrew.ryan@metoffice.gov.uk>
2020-09-09 11:21:38 +0100

    Add a checkbox to toggle coastlines

--- client/src/LayerMenu.js
+++ client/src/LayerMenu.js
@@ -1,11 +1,13 @@
 import React from "react"
 import { connect } from "react-redux"
-import { setActive } from "./actions.js"
+import { setActive, setFlag } from "./actions.js"
 
 
 class LayerMenu extends React.Component {
     render() {
         if (!this.props.visible) return null
+
+        // Datasets toggles
         const items = this.props.items
         const listItems = items.map(item => {
             const onChange = this.handleChange(item)
@@ -13,6 +15,19 @@ class LayerMenu extends React.Component {
                          label={ item.label }
                          onChange={ onChange } />
         })
+
+        // Coastlines toggle
+        const onChange = ev => {
+            const action = setFlag({
+                coastlines: ev.target.checked
+            })
+            this.props.dispatch(action)
+        }
+        listItems.push(
+            <Item key="coastlines" label="Coastlines"
+                  onChange={ onChange } />
+        )
+
         return <fieldset>{ listItems }</fieldset>
     }
 
