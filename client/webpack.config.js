a4c4e330b2436549985df517efea90d2d1436ce2
andrewgryan <andrew.ryan@metoffice.gov.uk>
2020-09-16 15:39:57 +0100

    edit webpack config js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
     ],
     output: {
         filename: '[name].min.js',
-        path: path.resolve(__dirname, "dist")
+        path: path.resolve(__dirname, "static")
     },
     module: {
         rules: [{
