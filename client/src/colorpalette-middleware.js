d9d99025315c4efabcda4b1c3eb44887bb7cf05b
andrewgryan <andrew.ryan@metoffice.gov.uk>
2020-09-22 18:05:22 +0100

    upgrade color palette middleware to try to match number/name palette combination

--- client/src/colorpalette-middleware.js
+++ client/src/colorpalette-middleware.js
@@ -11,6 +11,7 @@ import {
     set_palette_number,
     set_palette_numbers
 } from "./actions.js"
+import * as R from "ramda"
 
 
 /**
@@ -29,6 +30,21 @@ export const colorPaletteMiddleware = store => next => action => {
                 .sort((a, b) => a - b)
             let action = set_palette_numbers(numbers)
             store.dispatch(action)
+
+            // Set colors if palette name changes and number is valid
+            const { palettes, palette_number: number } = state
+            const hasName = R.propEq("name", name)
+            const hasNumber = R.propSatisfies(
+                x => parseInt(x) === parseInt(number),
+                "number")
+            const colors = R.pipe(
+                R.filter(R.both(hasName, hasNumber)),
+                R.map(R.prop("palette"))
+            )(palettes)
+            if (colors.length > 0) {
+                action = set_palette(colors[0])
+                store.dispatch(action)
+            }
         }
     }
     else if (action.type == SET_PALETTE_NUMBER) {
