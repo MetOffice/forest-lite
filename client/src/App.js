632f040b9d198d23f509f8326e08d68e980fef0b
andrewgryan <andrew.ryan@metoffice.gov.uk>
2020-10-06 10:54:19 +0100

    some null checks to prevent failure

--- client/src/App.js
+++ client/src/App.js
@@ -4,30 +4,25 @@ import MapFigure from "./MapFigure.js"
 import Title from "./Title.js"
 import Colorbar from "./Colorbar.js"
 import LayerMenu from "./LayerMenu.js"
-import LocalStorage from "./LocalStorage.js"
 import ViewPort from "./ViewPort.js"
 import ZoomButton from "./ZoomButton.js"
 
 
-class App extends React.Component {
-    render() {
-        const { baseURL } = this.props
-        return (
-            <div>
-                <LocalStorage />
-                <Title />
-                <LayerMenu baseURL={ baseURL } />
-                <div className="footer-container">
-                    <Colorbar />
-                    <AnimationControls />
-                </div>
-                <MapFigure baseURL={ baseURL } />
-                <ViewPort baseURL={ baseURL } />
-                <ZoomButton />
+const App = (props) => {
+    const { baseURL } = props
+    return (
+        <div>
+            <Title />
+            <LayerMenu baseURL={ baseURL } />
+            <div className="footer-container">
+                <Colorbar />
+                <AnimationControls />
             </div>
-        )
-    }
+            <MapFigure baseURL={ baseURL } />
+            <ViewPort baseURL={ baseURL } />
+            <ZoomButton />
+        </div>
+    )
 }
-
 
 export default App
