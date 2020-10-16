5a61813ce25c11c119842a1b8325f9cb5c276bac
andrewgryan <andrew.ryan@metoffice.gov.uk>
2020-10-16 09:33:45 +0100

    add support for NamedPalette into config

--- server/lib/config.py
+++ server/lib/config.py
@@ -1,6 +1,7 @@
 """Configuration parsing"""
-from pydantic import BaseModel
-from typing import List, Dict
+import bokeh.palettes
+from pydantic import BaseModel, validator
+from typing import List, Dict, Union
 
 
 class Viewport(BaseModel):
@@ -20,10 +21,27 @@ class Palette(BaseModel):
     high: float = 1
 
 
+class NamedPalette(BaseModel):
+    name: str
+    number: int
+    low: float = 0
+    high: float = 1
+
+    def palette(self):
+        colors = bokeh.palettes.all_palettes[self.name][self.number]
+        return Palette(colors=colors, low=self.low, high=self.high)
+
+
 class Dataset(BaseModel):
     label: str
     driver: Driver = Driver()
     palettes: Dict[str, Palette] = {}
+
+    @validator("palettes", pre=True, each_item=True)
+    def support_named_palettes(cls, v):
+        if "name" in v:
+            return NamedPalette(**v).palette()
+        return v
 
 
 class Config(BaseModel):
--- server/test_config.py
+++ server/test_config.py
@@ -1,4 +1,6 @@
 import lib.config
+from lib.config import Palette, NamedPalette
+import bokeh.palettes
 
 
 def test_config_object():
@@ -13,3 +15,29 @@ def test_config_dataset():
                                  driver={"name": "EIDA50", "settings": {}})
     assert dataset.label == "Label"
     assert dataset.driver == lib.config.Driver(name="EIDA50", settings={})
+
+
+def test_config_named_palette():
+    name = "Blues"
+    number = 3
+    result = NamedPalette(name=name, number=number).palette().colors
+    expect = list(bokeh.palettes.all_palettes[name][number])
+    assert result == expect
+
+
+def test_config_given_named_palette():
+    name = "Blues"
+    number = 3
+    config = lib.config.Config(
+        datasets=[
+            {
+                "label": "Name",
+                "palettes": {
+                    "key": { "name": name, "number": number }
+                }
+            }
+        ]
+    )
+    result = config.datasets[0].palettes["key"].dict()["colors"]
+    expect = list(bokeh.palettes.all_palettes[name][number])
+    assert result == expect
