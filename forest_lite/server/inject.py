ff5ede8c0aef40c5cea40d0eba4592a820def1a5
andrewgryan <andrew.ryan@metoffice.gov.uk>
2020-10-22 12:32:15 +0100

    add a Injectable parent class to allow decorator style injection

--- forest_lite/server/inject.py
+++ forest_lite/server/inject.py
@@ -1,5 +1,19 @@
+"""Simple dependency injection framework"""
 
 
 class Use:
+    """Container to delay execution of dependency"""
     def __init__(self, dependency):
         self.dependency = dependency
+
+
+class Injectable:
+    """Add function signature overriding"""
+    def __init__(self):
+        self.overridden_methods = []
+
+    def override(self, method_name):
+        def decorator(fn):
+            setattr(self, method_name, fn)
+            return fn
+        return decorator
--- forest_lite/test/test_drivers_injection.py
+++ forest_lite/test/test_drivers_injection.py
@@ -1,7 +1,21 @@
-from forest_lite.server.inject import Use
-from unittest.mock import sentinel
+from forest_lite.server.inject import Use, Injectable
+from unittest.mock import sentinel, Mock
 
 
 def test_dependency_injection_container():
     container = Use(sentinel.callback)
     assert container.dependency == sentinel.callback
+
+
+def test_override_mechanism():
+    custom_fn = Mock()
+
+    class Driver(Injectable):
+        def method(self):
+            raise Exception
+
+    driver = Driver()
+    driver.override("method")(custom_fn)
+    driver.method()
+
+    custom_fn.assert_called_once_with()
