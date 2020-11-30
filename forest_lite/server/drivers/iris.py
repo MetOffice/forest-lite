f9ee622649ba8b0fb0a13ee75438f59e98b3b1e6
andrewgryan <andrew.ryan@metoffice.gov.uk>
2020-11-30 17:18:40 +0000

    use unrotate_pole for UKV grid

--- forest_lite/server/drivers/iris.py
+++ forest_lite/server/drivers/iris.py
@@ -1,6 +1,7 @@
 """Support for iris loaded data"""
 import numpy as np
 import iris
+from iris.analysis.cartography import unrotate_pole
 from functools import lru_cache
 from forest_lite.server.util import get_file_names
 from forest_lite.server.drivers import BaseDriver
@@ -17,14 +18,23 @@ def tilable(settings, data_var, query=None):
     cube = cubes[0]
     lats = cube.coord("grid_latitude")[:].points
     lons = cube.coord("grid_longitude")[:].points.copy()
-    values = cube[0, 0].data.copy()
-    print(lons, lats)
-    # Roll input data into [-180, 180] range
-    if np.any(lons > 180.0):
-        shift_by = np.sum(lons > 180.0)
-        lons[lons > 180.0] -= 360.
-        lons = np.roll(lons, shift_by)
-        values = np.roll(values, shift_by, axis=1)
+
+    values = cube[0, -1].data.copy()
+    print(lons.shape, lats.shape)
+
+    # UKV Rotated pole support
+    rotated_lons, rotated_lats = np.meshgrid(lons, lats)
+    coord_system = cube.coord_system()
+    pole_lon = coord_system.grid_north_pole_longitude
+    pole_lat = coord_system.grid_north_pole_latitude
+    lons, lats = unrotate_pole(rotated_lons, rotated_lats, pole_lon, pole_lat)
+
+    # # Roll input data into [-180, 180] range
+    # if np.any(lons > 180.0):
+    #     shift_by = np.sum(lons > 180.0)
+    #     lons[lons > 180.0] -= 360.
+    #     lons = np.roll(lons, shift_by)
+    #     values = np.roll(values, shift_by, axis=1)
     return {
         "latitude": lats,
         "longitude": lons,
