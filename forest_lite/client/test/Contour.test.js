4259a3090176f0d96045d1891a1d10b01acba251
andrewgryan <andrew.ryan@metoffice.gov.uk>
2020-12-08 15:35:59 +0000

    add tests to check Contours component

--- forest_lite/client/test/Contour.test.js
+++ forest_lite/client/test/Contour.test.js
@@ -1,18 +1,41 @@
-import React from "react"
+import React, { useEffect, useRef } from "react"
 import { Provider } from "react-redux"
-import { render, screen, fireEvent } from "@testing-library/react"
+import { render, screen, fireEvent, waitFor } from "@testing-library/react"
 import "@testing-library/jest-dom/extend-expect"
 import { createStore } from "../src/create-store.js"
 import * as Bokeh from "@bokeh/bokehjs"
+import { act } from "react-dom/test-utils"
 import Contours from "../src/Contours.js"
+import { server } from "./server.js"
 
 
-test("Contours", () => {
+// Configure test suite
+beforeAll(() => server.listen())
+afterAll(() => server.close())
+afterEach(() => server.resetHandlers())
+
+
+test("Contours", async () => {
     const store = createStore()
-    const figure = Bokeh.Plotting.figure()
-    render(
-        <Provider store={ store } >
-            <Contours figure={ figure } />
-        </Provider>
-    )
+    const figure = Bokeh.Plotting.figure({
+        x_range: new Bokeh.Range1d({ start: 0, end: 1 }),
+        y_range: new Bokeh.Range1d({ start: 0, end: 1 }),
+    })
+    await act(async () => {
+        render(
+            <Provider store={ store }>
+                <Contours figure={ figure } />
+            </Provider>
+        )
+    })
+    expect(figure.renderers.length).toEqual(1)
+    const source = figure.renderers[0].data_source
+    expect(source.data).toEqual({
+        x: [NaN],
+        y: [NaN],
+        z: [NaN],
+        xs: [[0, 0]],
+        ys: [[0, 0]],
+        zs: [[NaN, NaN]],
+    })
 })
--- forest_lite/client/test/bokehjs.test.js
+++ forest_lite/client/test/bokehjs.test.js
@@ -2,6 +2,36 @@
 import * as Bokeh from "@bokeh/bokehjs"
 
 
+// Example to demonstrate testing Bokeh views
+test("Bokeh.Plotting.show", async () => {
+    const el = document.createElement("div")
+    const data = {
+        x: [4, 5, 6],
+        y: [7, 8, 9]
+    }
+    const source = new Bokeh.ColumnDataSource({ data })
+    const figure = Bokeh.Plotting.figure({
+        plot_width: 100,
+        plot_height: 100,
+        x_range: new Bokeh.Range1d({ start: 0, end: 4 }),
+        y_range: new Bokeh.Range1d({ start: 0, end: 4 })
+    })
+    const renderer = figure.circle({
+        x: { field: "x" },
+        y: { field: "y" },
+        source: source
+    })
+    await Bokeh.Plotting.show(figure, el)
+
+    // Note: assertions can be easily made against primitive values
+    //       bokeh objects sometimes raise errors
+    expect(renderer.visible).toEqual(true)
+    expect(renderer.data_source.data).toEqual(data)
+    expect(figure.plot_height).toEqual(100)
+    expect(figure.renderers[0]).toBe(renderer)
+})
+
+
 test("figure.x_range.start change", () => {
     const listener = jest.fn()
     const x_range = new Bokeh.Range1d({ start: 0, end: 2 })
