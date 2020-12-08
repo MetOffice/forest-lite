de4ce432b37994c29a3f4a72d19e6541f5f5aae6
andrewgryan <andrew.ryan@metoffice.gov.uk>
2020-12-08 16:26:29 +0000

    add a system test to capture existing contour behaviour

--- forest_lite/client/test/Contour.test.js
+++ forest_lite/client/test/Contour.test.js
@@ -6,6 +6,7 @@ import { createStore } from "../src/create-store.js"
 import * as Bokeh from "@bokeh/bokehjs"
 import { act } from "react-dom/test-utils"
 import Contours from "../src/Contours.js"
+import { setState } from "../src/actions.js"
 import { server } from "./server.js"
 
 
@@ -16,7 +17,23 @@ afterEach(() => server.resetHandlers())
 
 
 test("Contours", async () => {
+    const baseURL = "/contours"
+
+    // Configure store
     const store = createStore()
+    store.dispatch(setState({
+        contours: true,
+        dataset: 0,
+        times: [0],
+        time_index: 0
+    }))
+    const state = store.getState()
+    expect(state.contours).toEqual(true)
+    expect(state.dataset).toEqual(0)
+    expect(state.times).toEqual([0])
+    expect(state.time_index).toEqual(0)
+
+    // Configure Bokeh Figure
     const figure = Bokeh.Plotting.figure({
         x_range: new Bokeh.Range1d({ start: 0, end: 1 }),
         y_range: new Bokeh.Range1d({ start: 0, end: 1 }),
@@ -24,18 +41,32 @@ test("Contours", async () => {
     await act(async () => {
         render(
             <Provider store={ store }>
-                <Contours figure={ figure } />
+                <Contours figure={ figure } baseURL={ baseURL } />
             </Provider>
         )
     })
-    expect(figure.renderers.length).toEqual(1)
     const source = figure.renderers[0].data_source
+    const xs00 = 14026255.83995247 // Sample contour
+    await waitFor(() => {
+        expect(source.data.xs[0][0]).toEqual(xs00)
+    })
+    expect(figure.renderers.length).toEqual(1)
     expect(source.data).toEqual({
-        x: [NaN],
-        y: [NaN],
-        z: [NaN],
-        xs: [[0, 0]],
-        ys: [[0, 0]],
-        zs: [[NaN, NaN]],
+        value: [290, 300],
+        x: [NaN, NaN],
+        y: [NaN, NaN],
+        z: [NaN, NaN],
+        xs: [
+            [14026255.83995247, 13914936.349159196],
+            [14026255.83995247, 13995895.978827031]
+        ],
+        ys: [
+            [6874936.917757867, 6952519.1760577075],
+            [6958060.765936268, 6982997.920389787]
+        ],
+        zs: [
+            [NaN, NaN],
+            [NaN, NaN]
+        ],
     })
 })
