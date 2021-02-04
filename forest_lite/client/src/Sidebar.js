d5ffba8eded2e77e3499237d000c77819bf87150
andrewgryan <andrew.ryan@metoffice.gov.uk>
2021-02-04 14:45:16 +0000

    remove unused javascript imports

--- forest_lite/client/src/Sidebar.js
+++ forest_lite/client/src/Sidebar.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react"
 import { connect } from "react-redux"
 import "./Sidebar.css"
-import Tab from "./Tab.js"
-import Nav from "./Nav.js"
-import DatasetsMenu from "./DatasetsMenu.js"
+import "./Tab.css"
+import "./Nav.css"
+import "./Select.css"
+import "./DatasetsMenu.css"
 import Hamburger from "./Hamburger.js"
 
 const Sidebar = ({ baseURL, children }) => {
@@ -16,11 +17,6 @@ const Sidebar = ({ baseURL, children }) => {
     if (isOpen) {
         className = "Sidebar--visible"
     }
-    //                <div className="Sidebar__row">
-    //                    <DatasetsMenu />
-    //                    <ShowLayer />
-    //                </div>
-    //                <Nav baseURL={ baseURL } />
     return (<div className="layer-menu-container">
             <Hamburger onClick={ onClick } />
             <div className={ className }>
