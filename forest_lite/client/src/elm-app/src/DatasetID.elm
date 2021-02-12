e5db6d0474257e244725f725cd282e03d3e6a106
andrewgryan <andrew.ryan@metoffice.gov.uk>
2021-02-12 19:04:57 +0000

    move DatasetID into its own module

---
+++ forest_lite/client/src/elm-app/src/DatasetID.elm
@@ -0,0 +1,30 @@
+module DatasetID exposing (DatasetID(..), decoder, encode, toInt, toString)
+
+import Json.Decode
+import Json.Encode
+
+
+type DatasetID
+    = DatasetID Int
+
+
+encode : DatasetID -> Json.Encode.Value
+encode id =
+    Json.Encode.int (toInt id)
+
+
+decoder : Json.Decode.Decoder DatasetID
+decoder =
+    Json.Decode.map
+        DatasetID
+        Json.Decode.int
+
+
+toInt : DatasetID -> Int
+toInt (DatasetID n) =
+    n
+
+
+toString : DatasetID -> String
+toString id =
+    String.fromInt (toInt id)
