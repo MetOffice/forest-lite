0b03436310544e919ffd2ba9ad31ced850642fed
andrewgryan <andrew.ryan@metoffice.gov.uk>
2021-03-01 14:47:30 +0000

    wire everything up after ports set

--- forest_lite/client/src/OnPanZoom.js
+++ forest_lite/client/src/OnPanZoom.js
@@ -1,33 +1,45 @@
 import React, { useEffect } from "react"
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import debounce from "lodash/debounce"
 import { makeOnPanZoom } from "./on-pan-zoom.js"
 import { setFigure } from "./actions.js"
+import { selectPorts } from "./ports-selector.js"
 
 
+/**
+ * Send messages with Bokeh axis extents
+ */
 const OnPanZoom = ({ figure }) => {
+    const ports = useSelector(selectPorts)
     const dispatch = useDispatch()
     useEffect(() => {
-        // Listen to x_range.start changes
-        let eventHandler = makeOnPanZoom(figure.x_range)
-        let fn = () => {
+        if (ports != null) {
+            // Listen to x_range.start changes
+            let eventHandler = makeOnPanZoom(figure.x_range)
+            let fn = () => {
 
-            // Set figure axis limits in application state
-            const props = {
-                x_range: {
-                    start: figure.x_range.start,
-                    end: figure.x_range.end
-                },
-                y_range: {
-                    start: figure.y_range.start,
-                    end: figure.y_range.end
+                // Set figure axis limits in application state
+                const props = {
+                    x_range: {
+                        start: figure.x_range.start,
+                        end: figure.x_range.end
+                    },
+                    y_range: {
+                        start: figure.y_range.start,
+                        end: figure.y_range.end
+                    }
                 }
+                const action = setFigure(props)
+
+                // Send figure extent to Elm
+                ports.receiveData.send({ label: "action", payload: action })
+
+                // Send figure extent to React-Redux
+                dispatch(action)
             }
-            const action = setFigure(props)
-            dispatch(action)
+            eventHandler(debounce(fn, 400))
         }
-        eventHandler(debounce(fn, 400))
-    }, [])
+    }, [ ports ])
     return null
 }
 
