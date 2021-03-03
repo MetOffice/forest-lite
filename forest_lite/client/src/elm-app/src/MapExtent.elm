8cfe0682b3492581b60e9ef00e71f5f4c3500620
andrewgryan <andrew.ryan@metoffice.gov.uk>
2021-03-03 17:16:09 +0000

    implement transform from WebMercator to WGS84 projection

--- forest_lite/client/src/elm-app/src/MapExtent.elm
+++ forest_lite/client/src/elm-app/src/MapExtent.elm
@@ -9,3 +9,59 @@ type MapExtent
 init : MapExtent
 init =
     NotReady
+
+
+
+-- WEB MERCATOR
+
+
+type WGS84
+    = WGS84 Float Float
+
+
+type WebMercator
+    = WebMercator Float Float
+
+
+earthRadius : Float
+earthRadius =
+    6378137.0
+
+
+xLimits : ( Float, Float )
+xLimits =
+    ( -pi * earthRadius, pi * earthRadius )
+
+
+yLimits : ( Float, Float )
+yLimits =
+    ( -pi * earthRadius, pi * earthRadius )
+
+
+toWGS84 : WebMercator -> WGS84
+toWGS84 (WebMercator x y) =
+    WGS84 (toLon x) (toLat y)
+
+
+toLon : Float -> Float
+toLon x =
+    toDeg (x / earthRadius)
+
+
+toLat : Float -> Float
+toLat y =
+    toDeg (atan (sinh (y / earthRadius)))
+
+
+toDeg : Float -> Float
+toDeg radians =
+    radians * (180 / pi)
+
+
+
+-- MATH
+
+
+sinh : Float -> Float
+sinh x =
+    (e ^ x - e ^ -x) / 2
