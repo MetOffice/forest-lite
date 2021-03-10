fba6de38e9a6e55433a2c5f57909d272ef31d6fd
andrewgryan <andrew.ryan@metoffice.gov.uk>
2021-03-10 18:00:35 +0000

    add a simple indexed DB objectStore

--- forest_lite/client/src/web-worker.js
+++ forest_lite/client/src/web-worker.js
@@ -1,7 +1,50 @@
 /**
  * Web worker to perform off-main thread tasks
  */
+
+
+// IndexedDB set up
+const version = 1
+const dbName = "forest-lite"
+const request = indexedDB.open(dbName, version)
+request.onerror = ev => {
+    console.log("error")
+}
+request.onsuccess = ev => {
+    console.log("success")
+}
+request.onupgradeneeded = ev => {
+    const db = ev.target.result
+    const objectStore = db.createObjectStore("natural_earth_feature",
+                                             { keyPath: ["feature", "quadkey"]})
+
+    objectStore.createIndex("feature", "feature", { unique: false })
+
+    objectStore.createIndex("quadkey", "quadkey", { unique: false })
+
+    objectStore.transaction.oncomplete = ev => {
+        console.log("upgrade complete")
+    }
+}
+
+/**
+ * Receive messages from main thread
+ */
 onmessage = ({ data }) => {
     const { type, payload } = data
-    console.log("worker.js", payload)
+
+    // Add an entry to IndexedDB
+    const request = indexedDB.open(dbName, version)
+    request.onsuccess = ev => {
+        const db = ev.target.result
+        const transaction = db.transaction([ "natural_earth_feature" ], "readwrite")
+        const objectStore = transaction.objectStore("natural_earth_feature")
+        const request = objectStore.add(payload)
+        request.onerror = ev => {
+            console.log("failed to add record")
+        }
+        request.onsuccess = ev => {
+            console.log("successfully added record")
+        }
+    }
 }
