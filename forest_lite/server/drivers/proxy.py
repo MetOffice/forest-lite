84abf63fc580c9a9e384047dbf625695c99657b7
andrewgryan <andrew.ryan@metoffice.gov.uk>
2021-07-01 09:09:09 +0100

    add dataset_id to proxy driver

--- forest_lite/server/drivers/proxy.py
+++ forest_lite/server/drivers/proxy.py
@@ -10,6 +10,7 @@ from forest_lite.server.drivers.base import BaseDriver
 
 class Settings(BaseModel):
     url: str
+    dataset_id: int
 
 
 driver = BaseDriver()
@@ -18,7 +19,8 @@ driver = BaseDriver()
 @driver.override("description")
 async def description(settings_dict):
     url = Settings(**settings_dict).url
-    endpoint = f"{url}/datasets/0"
+    dataset_id = Settings(**settings_dict).dataset_id
+    endpoint = f"{url}/datasets/{dataset_id}"
     async with aiohttp.ClientSession() as session:
         async with session.get(endpoint) as response:
             content = await response.json()
@@ -28,7 +30,8 @@ async def description(settings_dict):
 @driver.override("points")
 async def points(settings_dict, data_var, dim_name, query=None):
     url = Settings(**settings_dict).url
-    endpoint = f"{url}/datasets/0/{data_var}/axis/{dim_name}"
+    dataset_id = Settings(**settings_dict).dataset_id
+    endpoint = f"{url}/datasets/{dataset_id}/{data_var}/axis/{dim_name}"
     async with aiohttp.ClientSession() as session:
         async with session.get(endpoint) as response:
             content = await response.json()
@@ -38,7 +41,7 @@ async def points(settings_dict, data_var, dim_name, query=None):
 @driver.override("data_tile")
 async def data_tile(settings_dict, data_var, z, x, y, query=None):
     url = Settings(**settings_dict).url
-    dataset_id = 0
+    dataset_id = Settings(**settings_dict).dataset_id
     endpoint = f"{url}/datasets/{dataset_id}/{data_var}/tiles/{z}/{x}/{y}"
     if query is not None:
         query = urllib.parse.quote(json.dumps(query))
